9c32d07479dd2320c55d92bc3d53b403fb68151b
florardu <andretabourian@hotmail.com>
2021-04-29 04:44:43 +0300

    Kernel 3 does not work :(

--- kernel3.cu
+++ kernel3.cu
@@ -65,21 +65,9 @@ __global__ void nw_kernel3(unsigned char* reference, unsigned char* query, int* 
     // Initialise Shared Memory
     __shared__ int matrix_s[BLOCK_SIZE][BLOCK_SIZE];
 
-    // Setup thread activation
-    if(  )
-    for( int i=0; i < BLOCK_SIZE; i++ ) {
+    if( threadIdx.y == 0 ) {
 
-
-
-    }
-
-    bool isThreadActive = (threadIdx.x + threadIdx.y) % 2 == 0;
-    bool shouldThreadLoadNewValue = false; 
- 
-
-    if( threadIdx.x < BLOCK_SIZE ) {
-
-        for( int diagonal = 0; diagonal < BLOCK_SIZE; diagonal++ ) {
+        for( int diagonal=0; diagonal < BLOCK_SIZE; diagonal++ ) {
 
             // Get the position of the thread inside the block.
             int pos_x = threadIdx.x;
@@ -92,85 +80,227 @@ __global__ void nw_kernel3(unsigned char* reference, unsigned char* query, int* 
             if( mat_row < N && mat_col < N && pos_x < BLOCK_SIZE && pos_y < BLOCK_SIZE && pos_x >= 0 && pos_y >= 0) { 
 
                 private_nw_function_3(reference, query, matrix, N, matrix_s, pos_x, pos_y, mat_row, mat_col); 
-              
+            
             }
 
         }
 
     }
 
-    __syncthreads(); 
+    __syncthreads();
 
-    int diagFirstPos = (threadIdx.x / BLOCK_SIZE) * 2;
-    bool isSecondDiag = false;
+    bool isThreadActive = (threadIdx.x + threadIdx.y) % 2 == 0;
 
-    int diagPos = diagFirstPos + isSecondDiag;
+    int row = 0;
+    int col = 0;
+    int diag = 0;
 
-    pos_x = threadIdx.x;
-    pos_y = diagonalPos - pos_x;
+    for( int subBlock = 1; subBlock < COVERAGE * COVERAGE; subBlock++ ) {
 
-    if( pos_x < BLOCK_SIZE && pos_y < BLOCK_SIZE && pos_x >= 0 && pos_y >= 0  ) {
+        row++;
+        col--;
 
+        // AFTER GREATEST DIAGONAL
+        if( diag >= COVERAGE ) {
+            diag++;
+            row = diag - COVERAGE + 1; 
+            col = COVERAGE;
+        }
 
+        // RESET
 
-    }
+        if(col < 0) {
+            diag++;
+            row = 0;
+            col = diag;
+        }
 
-    isSecondDiag = !(isSecondDiag)
+        if ( diag >= COVERAGE && col < diag - COVERAGE + 1 ) {
+            diag++;
+            row = diag - COVERAGE + 1; 
+            col = COVERAGE; 
+        }
 
-    for(int i=0; i < COVERAGE * COVERAGE; i++) {
+        __syncthreads();
 
-        int output_row = 0;
+        // LOAD NEXT SUB-BLOCK
+        if( threadIdx.x == 0 && threadIdx.y == 0 ) {
 
-        for( int diagonal = 0; diagonal < BLOCK_SIZE; diagonal++ ) { 
-
-            // Get the position of the thread inside the block.
-            int pos_x = threadIdx.x;
-            int pos_y = diagonal - pos_x;
-            
             // Calculate the positions of the thread inside the matrix.
-            int mat_row = block_row + ( (i+1) / COVERAGE ) * BLOCK_SIZE + pos_y;
-            int mat_col = block_col + ( (i+1) % COVERAGE ) * BLOCK_SIZE + pos_x;
-            
-            if( threadIdx.x >= BLOCK_SIZE ) {
+            int mat_row = block_row + row * BLOCK_SIZE;
+            int mat_col = block_col + col * BLOCK_SIZE;
 
-                pos_x = 2 * (BLOCK_SIZE) - threadIdx.x - 1; 
-                pos_y = BLOCK_SIZE - pos_x + diagonal;
-               
-               // Calculate the positions of the thread inside the matrix.
-                mat_row = block_row + ( i / COVERAGE ) * BLOCK_SIZE + pos_y;
-                mat_col = block_col + ( i % COVERAGE ) * BLOCK_SIZE + pos_x;
-           
+            if ( mat_row < N && mat_col < N ) {
+                private_nw_function_3(reference, query, matrix, N, matrix_s, 0, 0, mat_row, mat_col);
             }
-     
-            if( threadIdx.x < BLOCK_SIZE ) {
-
-                    int row = block_row + ( i / COVERAGE ) * BLOCK_SIZE + output_row;
-                    int col = block_col + ( i % COVERAGE ) * BLOCK_SIZE + threadIdx.x;
-
-                    if( row < N && col < N ) {
-
-                        matrix[row * N + col] = matrix_s[output_row][threadIdx.x];
-
-                    }
-
-            }
-            
-            if( mat_row < N && mat_col < N && pos_x < BLOCK_SIZE && pos_y < BLOCK_SIZE && pos_x >= 0 && pos_y >= 0) {
-        
-                private_nw_function(reference, query, matrix, N, matrix_s, pos_x, pos_y, mat_row, mat_col);
-
-            }
-            
-            __syncthreads();
-    
-            output_row++;
 
         }
 
         __syncthreads();
 
+        int pos_x = threadIdx.x;
+        int pos_y = threadIdx.y;
+
+        int current_diagonal = (pos_x + pos_y) / 2;
+
+        int subBlock_row = row - current_diagonal;
+        int subBlock_col = diag - subBlock_row;
+
+        if( subBlock_row < 0 && diag < COVERAGE ) {
+
+            int num_jumps = (-1 * subBlock_row) - 1;
+            int diag_jumps = 1;
+
+            subBlock_row = diag - diag_jumps;
+            subBlock_col = 0;
+
+            while( num_jumps > 0 ) {
+            
+                subBlock_row--;
+                subBlock_col++;
+            
+                if( subBlock_row < 0 ) {
+
+                    diag_jumps++;
+
+                    subBlock_row = diag - diag_jumps;
+                    subBlock_col = 0;
+
+                }
+
+                num_jumps--;
+
+            }
+
+            if( subBlock == 9 ) {
+
+                printf("num_jumps: %d, x: %d, y: %d, row: %d, col: %d\n", num_jumps, threadIdx.x, threadIdx.y, subBlock_row, subBlock_col);
+    
+            }
+
+            if ( subBlock_row < 0 ) {
+                subBlock_row = 0;
+                subBlock_col = 0;
+            }
+
+        }
+
+        if( subBlock_row < diag - COVERAGE + 1 && diag >= COVERAGE ) {
+
+            int num_jumps = (diag - COVERAGE + 1) - subBlock_row - 1;
+            int diag_jumps = 1;
+
+            subBlock_row = COVERAGE;
+            subBlock_col = (diag - diag_jumps) - COVERAGE + 1;
+
+            while( num_jumps > 0 ) {
+            
+                subBlock_row--;
+                subBlock_col++;
+            
+                if( subBlock_row < diag - COVERAGE + 1 ) {
+
+                    diag_jumps++;
+
+                    subBlock_row = COVERAGE;
+                    subBlock_col = (diag - diag_jumps) - COVERAGE + 1;
+
+                }
+
+                if( diag_jumps <= 0 ) {
+                    subBlock_row = 0;
+                    subBlock_col = 0;
+                    break;
+                }
+
+                num_jumps--;
+
+            }
+
+            if ( subBlock_row < 0 ) {
+                subBlock_row = 0;
+                subBlock_col = 0;
+            }
+
+        }
+
+        __syncthreads();
+
+        int mat_row = block_row + subBlock_row * BLOCK_SIZE + pos_y;
+        int mat_col = block_col + subBlock_col * BLOCK_SIZE + pos_x;
+
+        // First Activation
+        if( isThreadActive && threadIdx.x != 0 && threadIdx.x != 0 ) {
+
+            // if( subBlock == 2 ) {
+            //     printf( "mat_col: %d\n", mat_col);
+            // }
+
+            if( mat_row < N && mat_col < N ) {
+
+                private_nw_function_3(reference, query, matrix, N, matrix_s, pos_x, pos_y, mat_row, mat_col);
+
+            }
+    
+        }
+
+        __syncthreads();
+
+        isThreadActive = !(isThreadActive);
+
+
+        // SECOND ACTIVATION
+        if( isThreadActive ) {
+
+            if( mat_row < N && mat_col < N ) {
+        
+                private_nw_function_3(reference, query, matrix, N, matrix_s, pos_x, pos_y, mat_row, mat_col);
+
+            }
+
+        }
+
+        isThreadActive = !(isThreadActive);
+
+        __syncthreads();
+
+        // if( subBlock == 11 && threadIdx.x == 0 && threadIdx.y == 0 ) {
+
+        //     printf("%d, %d", mat_row, mat_col);
+
+            // printf("\n");
+            // for( int i=32*0; i<32*1; i++ ) {
+            //     for( int j=32*1; j<32*2; j++ ) {
+            //         printf( "%d, ", matrix[i*N + j]);
+            //     }
+            //     printf("\n");
+            // }
+
+            // printf("\n");
+            // for( int i=0; i<32; i++ ) {
+            //     for( int j=0; j<32; j++ ) {
+            //         printf( "%d, ", matrix_s[i][j]);
+            //     }
+            //     printf("\n");
+            // }
+        // }
+
+
+        __syncthreads();
+
     }
 
+    if( threadIdx.x == 0 && threadIdx.y == 0 ) {
+
+        printf("\nResult:\n\n");
+        for( int i=32*0; i<32*1; i++ ) {
+            for( int j=32*0; j<32*1; j++ ) {
+                printf( "%d, ", matrix[i*N + j]);
+            }
+            printf("\n");
+        }
+
+    }
     
 
 }
@@ -180,14 +310,12 @@ void nw_gpu3(unsigned char* reference_d, unsigned char* query_d, int* matrix_d, 
     
     dim3 numThreadsPerBlock( BLOCK_SIZE, BLOCK_SIZE );
 
-    mm_tiled_kernel<<< numBlocks, numThreadsPerBlock>>>( A_d, B_d, C_d, M, N, K);
-
     for(int iter=0; iter < 2* ( (N + BLOCK_SIZE * COVERAGE - 1) / (BLOCK_SIZE * COVERAGE)) - 1; iter++) {
 
         // Configure next run
         unsigned int numBlocks = (iter < (N + BLOCK_SIZE * COVERAGE - 1) / (BLOCK_SIZE * COVERAGE)) ? (iter + 1) : (2*((N + BLOCK_SIZE * COVERAGE - 1) / (BLOCK_SIZE * COVERAGE)) - iter - 1);
 
-        //printf("iteration: %d, blocks: %d, threads: %d\n", iter, numBlocks, numThreadsPerBlock);
+        printf("iteration: %d, blocks: %d\n", iter, numBlocks);
         // Launch kernel
         nw_kernel3<<<numBlocks, numThreadsPerBlock>>>(reference_d, query_d, matrix_d, N, iter);
         
